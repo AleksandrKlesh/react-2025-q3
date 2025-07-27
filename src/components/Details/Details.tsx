8f3e450eaaeeedc890712965df7baf42ce05f890
AleksandrKlesh <alex73kl@gmail.com>
2025-07-28 01:31:34 +0700

    feat: add details pane and tests

---
+++ src/components/Details/Details.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import fetchCharacter from '../../services/fetchCharecter';
+import type { Character } from '../../types';
+
+export default function Details() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const id = Number(searchParams.get('details'));
+  const page = searchParams.get('page');
+
+  const [character, setCharacter] = useState<Character | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (id) {
+      loadCharacter(id);
+    }
+  }, [id]);
+
+  const loadCharacter = (id: number) => {
+    setLoading(true);
+    fetchCharacter(id)
+      .then((result) => setCharacter(result))
+      .finally(() => setLoading(false));
+  };
+
+  if (!id) return null;
+
+  return (
+    <div className="p4 border-1 h-full relative">
+      <button
+        className="absolute top-2 right-2 text-red-500"
+        onClick={() => navigate(`/?page=${page}`)}
+      >
+        Close
+      </button>
+      {loading ? (
+        <div className="text-blue-500">Loading details...</div>
+      ) : character ? (
+        <div>
+          <img src={character.image} alt={character.image} />
+          <h2 className="text-lg font-bold">{character.name}</h2>
+          <p>Species: {character.species}</p>
+          <p>Gender: {character.gender}</p>
+          <p>Type: {character.type}</p>
+          <p>Status:{character.status}</p>
+          <p>Origin: {character.origin?.name}</p>
+          <p>Location: {character.location?.name}</p>
+        </div>
+      ) : (
+        <div>No character found</div>
+      )}
+    </div>
+  );
+}
