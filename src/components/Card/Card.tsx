b054f680d6aa6200b1b39a6ffde57992ff687752
AleksandrKlesh <alex73kl@gmail.com>
2025-07-27 15:57:34 +0700

    refactor: migrate card class component to function

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface Props {
   name: string;
   species: string;
@@ -7,21 +5,19 @@ interface Props {
   image: string;
 }
 
-export default class Card extends React.Component<Props> {
-  render() {
-    return (
-      <div className="bg-white rounded-xl shadow-md p-4 mb-4 flex gap-4 items-center">
-        <img
-          className="w-24 h-24 rounded-full object-cover border border-gray-300"
-          src={this.props.image}
-          alt={this.props.name}
-        />
-        <div>
-          <h2 className="text-xl font-bold text-gray-800">{this.props.name}</h2>
-          <p className="text-sm text-gray-600">{this.props.species}</p>
-          <p className="text-sm text-gray-600">{this.props.gender}</p>
-        </div>
+export default function Card({ name, species, gender, image }: Props) {
+  return (
+    <div className="bg-white rounded-xl shadow-md p-4 mb-4 flex gap-4 items-center">
+      <img
+        className="w-24 h-24 rounded-full object-cover border border-gray-300"
+        src={image}
+        alt={name}
+      />
+      <div>
+        <h2 className="text-xl font-bold text-gray-800">{name}</h2>
+        <p className="text-sm text-gray-600">{species}</p>
+        <p className="text-sm text-gray-600">{gender}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
