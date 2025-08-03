1d9958c850e095c314520680baef52c85a11fae1
AleksandrKlesh <alex73kl@gmail.com>
2025-08-04 01:34:45 +0700

    feat: add themeContext, themeProvider and add theme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { ThemeContext, type ThemeContextType } from '../contexts/ThemeContext';
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+
+  if (!context) throw new Error('useTheme must be used within ThemeProvider');
+  return context;
+};
